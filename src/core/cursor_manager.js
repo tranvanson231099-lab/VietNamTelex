7525638a9e1516ed61e9a46690eb0c376d4a3a43
tranvanson231099 <tranvanson231099@gmail.com>
2026-04-25 14:32:56 +0000

    m

--- src/core/cursor_manager.js
+++ src/core/cursor_manager.js
@@ -4,20 +4,27 @@ export const CursorManager = {
     },
   
     getWord(text, cursor) {
-      if (!text) return "";
+      console.log("=== DEBUG GET WORD ===");
+      console.log("TEXT:", text);
+      console.log("CURSOR:", cursor);
+  
+      if (!text) {
+        console.log("EMPTY TEXT");
+        return "";
+      }
   
       let start = cursor;
       let end = cursor;
   
       // =====================
-      // 1. tìm start (lùi trái)
+      // find start
       // =====================
       while (start > 0 && this.isLetter(text[start - 1])) {
         start--;
       }
   
       // =====================
-      // 2. tìm end (tới phải)
+      // find end
       // =====================
       while (end < text.length && this.isLetter(text[end])) {
         end++;
@@ -25,19 +32,34 @@ export const CursorManager = {
   
       const word = text.slice(start, end);
   
+      console.log("START:", start);
+      console.log("END:", end);
+      console.log("WORD:", word);
+      console.log("=====================");
+  
       return word;
     },
   
     getRange(text, cursor) {
-      if (!text) return { start: cursor, end: cursor };
+      console.log("=== DEBUG RANGE ===");
+      console.log("TEXT:", text);
+      console.log("CURSOR:", cursor);
   
-      const isLetter = this.isLetter;
+      if (!text) {
+        console.log("EMPTY TEXT");
+        return { start: cursor, end: cursor };
+      }
   
       let start = cursor;
       let end = cursor;
   
-      while (start > 0 && isLetter(text[start - 1])) start--;
-      while (end < text.length && isLetter(text[end])) end++;
+      while (start > 0 && this.isLetter(text[start - 1])) start--;
+      while (end < text.length && this.isLetter(text[end])) end++;
+  
+      console.log("START:", start);
+      console.log("END:", end);
+      console.log("RESULT:", text.slice(start, end));
+      console.log("=====================");
   
       return { start, end };
     }
