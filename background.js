b96d5bc829083ca316ff12062e01855f5fd6d81e
tranvanson231099 <tranvanson231099@gmail.com>
2026-05-02 05:48:31 +0000

    up

--- background.js
+++ background.js
@@ -1,35 +1,43 @@
 const engineID = "my_custom_ime";
 
-chrome.input.ime.onActivate.addListener((engineID) => {
-  console.log("onActivate", engineID);
-});
+// This will prevent the extension from crashing on non-ChromeOS devices
+// where the chrome.input.ime API is not available.
+if (chrome.input && chrome.input.ime) {
+  chrome.input.ime.onActivate.addListener((engineID) => {
+    console.log("onActivate", engineID);
+  });
 
-chrome.input.ime.onDeactivated.addListener((engineID) => {
-  console.log("onDeactivated", engineID);
-});
+  chrome.input.ime.onDeactivated.addListener((engineID) => {
+    console.log("onDeactivated", engineID);
+  });
 
-chrome.input.ime.onFocus.addListener((context) => {
-  console.log("onFocus", context.contextID);
-});
+  chrome.input.ime.onFocus.addListener((context) => {
+    console.log("onFocus", context.contextID);
+  });
 
-chrome.input.ime.onBlur.addListener((contextID) => {
-  console.log("onBlur", contextID);
-});
+  chrome.input.ime.onBlur.addListener((contextID) => {
+    console.log("onBlur", contextID);
+  });
 
-chrome.input.ime.onKeyEvent.addListener((engineID, keyData, requestId) => {
-  console.log("onKeyEvent", keyData);
-  if (keyData.type === "keydown") {
-    if (keyData.key.length === 1) {
+  chrome.input.ime.onKeyEvent.addListener((engineID, keyData, requestId) => {
+    console.log("onKeyEvent", keyData);
+    if (keyData.type === "keydown" && keyData.key.length === 1) {
       chrome.input.ime.commitText({
-        contextID: -1, // You might need to manage context ID here
+        // A context ID of 0 sends text to the currently focused text field.
+        contextID: 0,
         text: keyData.key,
       });
-      return true;
+      return true; // Indicates that the key event was handled.
     }
-  }
-  return false;
-});
+    return false; // Indicates the key event was not handled.
+  });
 
-chrome.input.ime.onMenuItemClicked.addListener((engineID, name) => {
-  console.log("onMenuItemClicked", engineID, name);
-});
+  chrome.input.ime.onMenuItemClicked.addListener((engineID, name) => {
+    console.log("onMenuItemClicked", engineID, name);
+  });
+
+  console.log("Custom IME listeners attached successfully.");
+
+} else {
+  console.log("chrome.input.ime API is not available. This extension's IME features are only functional on ChromeOS.");
+}
