3aadd8d2d35b7d76de4722934501ded3e467961e
tranvanson231099 <tranvanson231099@gmail.com>
2026-05-02 05:46:25 +0000

    up

--- background.js
+++ background.js
@@ -1,52 +1,35 @@
-const imeEngine = {
-    engineID: "my_custom_ime",
-    contextId: -1, 
-    menuItems: [
-        { id: "about", label: "About this IME" }
-    ],
+const engineID = "my_custom_ime";
 
-    onActivate: (engineID) => {
-        console.log("onActivate", engineID);
-    },
+chrome.input.ime.onActivate.addListener((engineID) => {
+  console.log("onActivate", engineID);
+});
 
-    onDeactivated: (engineID) => {
-        console.log("onDeactivated", engineID);
-    },
+chrome.input.ime.onDeactivated.addListener((engineID) => {
+  console.log("onDeactivated", engineID);
+});
 
-    onFocus: (context) => {
-        console.log("onFocus", context.contextID);
-        imeEngine.contextId = context.contextID;
-    },
+chrome.input.ime.onFocus.addListener((context) => {
+  console.log("onFocus", context.contextID);
+});
 
-    onBlur: (contextID) => {
-        console.log("onBlur", contextID);
-        if (imeEngine.contextId === contextID) {
-            imeEngine.contextId = -1;
-        }
-    },
+chrome.input.ime.onBlur.addListener((contextID) => {
+  console.log("onBlur", contextID);
+});
 
-    onKeyEvent: (engineID, keyData, requestId) => {
-        console.log("onKeyEvent", keyData);
-        if (keyData.type === "keydown" && imeEngine.contextId !== -1) {
-            if (keyData.key.length === 1) {
-                chrome.input.ime.commitText({
-                    contextID: imeEngine.contextId,
-                    text: keyData.key
-                });
-                return true;
-            }
-        }
-        return false;
-    },
-
-    onMenuItemClicked: (engineID, name) => {
-        console.log("onMenuItemClicked", engineID, name);
+chrome.input.ime.onKeyEvent.addListener((engineID, keyData, requestId) => {
+  console.log("onKeyEvent", keyData);
+  if (keyData.type === "keydown") {
+    if (keyData.key.length === 1) {
+      chrome.input.ime.commitText({
+        contextID: -1, // You might need to manage context ID here
+        text: keyData.key,
+      });
+      return true;
     }
-}
+  }
+  return false;
+});
 
-chrome.input.ime.onActivate.addListener(imeEngine.onActivate);
-chrome.input.ime.onDeactivated.addListener(imeEngine.onDeactivated);
-chrome.input.ime.onFocus.addListener(imeEngine.onFocus);
-chrome.input.ime.onBlur.addListener(imeEngine.onBlur);
-chrome.input.ime.onKeyEvent.addListener(imeEngine.onKeyEvent);
-chrome.input.ime.onMenuItemClicked.addListener(imeEngine.onMenuItemClicked);
+chrome.input.ime.onMenuItemClicked.addListener((engineID, name) => {
+  console.log("onMenuItemClicked", engineID, name);
+});
